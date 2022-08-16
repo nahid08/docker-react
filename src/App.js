940cb417dd1e4992357220236c8a428ac925778c
nahid08 <drmc.nahid@gmail.com>
2022-08-17 03:19:19 +0600

    change name

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> My name is Nahiddasd
+          Edit <code>src/App.js</code> My name is Nahid.
         </p>
         <a
           className="App-link"
